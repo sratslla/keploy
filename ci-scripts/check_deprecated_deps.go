9245b52f12b8dcc131d5b3671404472cc622bd6d
sratslla <shubhgaur.cse24@jecrc.ac.in>
2024-07-21 14:12:17 +0530

    added script to check for deprecated dependecy
    Signed-off-by: sratslla <shubhgaur.cse24@jecrc.ac.in>

--- ci-scripts/check_deprecated_deps.go
+++ ci-scripts/check_deprecated_deps.go
@@ -1,9 +1,8 @@
-// ci-scripts/check_deprecated_deps.go
+// check_deprecated_deps.go
 package main
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 	"strings"
 )
@@ -13,23 +12,13 @@ func main() {
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Println("Error running go list:", err)
-		os.Exit(1)
+		return
 	}
 
 	lines := strings.Split(string(output), "\n")
-	warnings := []string{}
 	for _, line := range lines {
 		if strings.Contains(line, "deprecated") {
-			warnings = append(warnings, line)
+			fmt.Println("Deprecated dependency found:", line)
 		}
-	}
-
-	if len(warnings) > 0 {
-		for _, warning := range warnings {
-			fmt.Println("::warning file=go.mod::Deprecated dependency found:", warning)
-		}
-		fmt.Printf("::set-output name=deprecated-warnings::%d deprecated dependencies found.\n", len(warnings))
-	} else {
-		fmt.Println("No deprecated dependencies found.")
 	}
 }
