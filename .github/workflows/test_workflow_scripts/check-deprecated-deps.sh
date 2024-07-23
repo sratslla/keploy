ec9c407527e1fe076d3da334bd57289eda9c0253
sratslla <shubhgaur.cse24@jecrc.ac.in>
2024-07-23 23:23:53 +0530

    trail10
    Signed-off-by: sratslla <shubhgaur.cse24@jecrc.ac.in>

--- .github/workflows/test_workflow_scripts/check-deprecated-deps.sh
+++ .github/workflows/test_workflow_scripts/check-deprecated-deps.sh
@@ -3,16 +3,16 @@
 # Run `go list -m -u all` to list all dependencies.
 output=$(go list -m -u all)
 
-
-# Checking for deprecated dependencies.
 found_deprecated=false
-echo "$output" | while IFS= read -r line; do
+
+while IFS= read -r line; do
     if [[ "$line" == *"deprecated"* ]]; then
         echo "Deprecated dependency found: $line"
         found_deprecated=true
     fi
-done 
-if $found_deprecated; then
+done <<< "$output"
+
+if [ "$found_deprecated" = true ]; then
     echo "Exiting with failure due to deprecated dependencies."
     exit 1
 fi
